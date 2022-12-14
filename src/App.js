cfb877e006f720d66074616dff10b8ac83a1c927
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-14 20:16:25 +0900

    주소 예외 처리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Board from "./components/Board";
 import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 import Profile from "./components/Profile";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Route path="/" element={<Header />} />
 				<Route path="/profile" element={<Profile />} />
 				<Route path="/board" element={<Board />} />
+				<Route path="*" element={<NotFound />} /> {/* 주소 예외 처리 */}
 			</Routes>
 		</div>
 	);
