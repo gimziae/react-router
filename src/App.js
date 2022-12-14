090916f821b283e955a43fd8f1f2d94d6c1068cf
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-14 19:40:16 +0900

    기본 라우트 구조

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Board from "./components/Board";
+import Profile from "./components/Profile";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-				<h1> sll</h1>
-			</header>
+			<nav>
+				<a href="/">홈페이지로 이동</a> <br />
+				<a href="/profile">프로필 페이지로 이동</a> <br />
+				<a href="/board">게시판 페이지로 이동</a>
+			</nav>
+			{/* 라우츠와 라우터는 한 묶음!! 같이 사용해야 한다. */}
+			<Routes>
+				{/* path 주소설정 element 보여줄 컨텐츠 */}
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/board" element={<Board />} />
+			</Routes>
 		</div>
 	);
 }
